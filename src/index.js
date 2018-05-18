8294d737cb25d9c0d2e87c5ae50ed1a201a81443
besarion123 <beso.natadze@gmail.com>
2018-05-18 16:40:40 +0400

    material ui add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 const styles = {
   fontFamily: 'sans-serif',
   textAlign: 'center',
@@ -12,7 +13,12 @@ const App = () => (
     <Hello x="beso2" />
     <h2>Start editing to see some magic happen {'\u2728'}</h2>
     <h1>reactjs testing...</h1>
+    {/*}ssss*/}
+    <MuiThemeProvider>
+      <MyAwesomeReactComponent />
+    </MuiThemeProvider>
   </div>
+  
 
 );
 
